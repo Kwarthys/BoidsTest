00e8e2d2ba5826cfe855dc64013c972bc9b17e3b
Kwarthys <Kwarthys@gmail.com>
2019-03-18 18:25:36 +0100

    init

---
+++ Assets/BoidController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour {
+
+    private GameController gameController;
+
+    private int index;
+
+    private Transform nose;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * 20;
+
+        nose = transform.Find("nose").GetComponent<Transform>();
+
+        rb = GetComponent<Rigidbody>();
+
+        index = Random.Range(-1000, 1000);
+    }
+
+    public void registerController(GameController gc)
+    {
+        gameController = gc;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        List<BoidController> others = gameController.getInRangeBoids(this);
+
+        rb.angularVelocity = Vector3.zero;
+
+        rb.velocity = 20 * transform.forward;
+
+        if (others != null)
+        {
+            Vector3 othersVelocity = new Vector3();
+
+            int sum = 0;
+
+            foreach (BoidController boid in others)
+            {
+                if(boid.rb != null)
+                {
+                    othersVelocity += boid.rb.velocity;
+                    sum++;
+
+                    flockForceTowards(boid);
+                    flockForceOutwards(boid);
+                }
+            }
+
+            othersVelocity /= sum;
+
+            followFlock(othersVelocity);
+        }
+
+        getBackToCenter();
+
+        randomInput();
+
+    }
+
+    private void getBackToCenter()
+    {
+        Vector3 centerPos = gameController.focusPoint.position;
+        Vector3 forceTowardsCenter = Vector3.ProjectOnPlane(centerPos-transform.position, transform.forward).normalized * Vector3.Distance(transform.position, centerPos) * Vector3.Distance(transform.position, centerPos) / 600;
+        rb.AddForceAtPosition(forceTowardsCenter, nose.position);
+    }
+
+    private void followFlock(Vector3 othersVelocity)
+    {
+        Vector3 force = (othersVelocity - rb.velocity).normalized * gameController.cohesionForce;
+
+        force = Vector3.ProjectOnPlane(force, transform.forward);
+
+        rb.AddForceAtPosition(force, nose.position);
+    }
+
+    private void flockForceTowards(BoidController boid)
+    {
+        Vector3 force = (boid.transform.position - transform.position).normalized * gameController.linkingForce;
+
+        force = Vector3.ProjectOnPlane(force, transform.forward);
+
+        rb.AddForceAtPosition(force, nose.position);
+    }
+
+    private void flockForceOutwards(BoidController boid)
+    {
+        float weight = (gameController.detectionRange - Vector3.Distance(boid.transform.position, transform.position)) * gameController.explosionForce;
+
+        Vector3 force = - (boid.transform.position - transform.position).normalized * weight;
+
+        force = Vector3.ProjectOnPlane(force, transform.forward);
+
+        rb.AddForceAtPosition(force, nose.position);
+    }
+
+    private void randomInput()
+    {
+        Vector3 force = new Vector3(Mathf.PerlinNoise(index*1f / 500f, 0) * 2 - 1, Mathf.PerlinNoise((index+5)*1f / 500f, 0) * 2 - 1, Mathf.PerlinNoise((index+20)*1f / 500f, 0) * 2 - 1);
+
+        force = Vector3.ProjectOnPlane(force, transform.forward) * gameController.randomForce;
+
+        rb.AddForceAtPosition(force, nose.position);
+
+        index++;
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject boidPrefab;
+
+    public int flockSize = 2;
+
+    public float detectionRange = 80;
+    public float detectionAngle = 60;
+
+    public int spawnArea = 20;
+
+    public Camera cam;
+
+    private BoidController[] boidsList;
+
+    private Dictionary<BoidController, List<BoidController>> boidsDetection = new Dictionary<BoidController, List<BoidController>>();
+
+    private int index = 0;
+
+
+    [Header("Boids Rules")]
+    public int cohesionForce = 5;
+    public int linkingForce = 5;
+    public int explosionForce = 15;
+    public int randomForce = 15;
+
+    public Transform focusPoint;
+
+    public void Start()
+    {
+        boidsList = new BoidController[flockSize];
+
+        GameObject boid = Instantiate(boidPrefab, new Vector3(Random.Range(-spawnArea, spawnArea), Random.Range(-spawnArea, spawnArea), Random.Range(-spawnArea, spawnArea)), Random.rotation);
+        BoidController bc = boid.GetComponent<BoidController>();
+        bc.registerController(GetComponent<GameController>());
+        boidsList[0] = bc;
+        boid.name = "Original";
+
+        for (int i = 1; i < flockSize; i++)
+        {
+            boid = Instantiate(boid, new Vector3(Random.Range(-spawnArea, spawnArea), Random.Range(-spawnArea, spawnArea), Random.Range(-spawnArea, spawnArea)), Random.rotation);
+
+            bc = boid.GetComponent<BoidController>();
+
+            bc.registerController(GetComponent<GameController>());
+
+            boidsList[i] = bc;
+
+            boid.name = "Boid" + i;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        boidsDetection.Clear();
+
+        proceduralDetection();
+
+        moveCamera();
+    }
+
+    private void proceduralDetection()
+    {
+        int offset = 1;
+        for (int iBoidA = 0; iBoidA < boidsList.Length; iBoidA++)
+        {
+            for (int iBoidB = offset; iBoidB < boidsList.Length; iBoidB++)
+            {
+                if (inRange(boidsList[iBoidA], boidsList[iBoidB]))
+                {
+                    if (isDetected(boidsList[iBoidA], boidsList[iBoidB]))
+                    {
+                        addToDetecs(boidsList[iBoidA], boidsList[iBoidB]);
+                        //Debug.Log(index + " - " + boidsList[iBoidA].name + " detects " + boidsList[iBoidB].name);
+                    }
+
+                    if (isDetected(boidsList[iBoidB], boidsList[iBoidA]))
+                    {
+                        addToDetecs(boidsList[iBoidB], boidsList[iBoidA]);
+                        //Debug.Log(index + " - " + boidsList[iBoidB].name + " detects " + boidsList[iBoidA].name);
+                    }
+                }
+            }
+
+            offset++;
+        }
+        index++;
+    }
+
+    private void addToDetecs(BoidController detector, BoidController detected)
+    {
+        if (boidsDetection.ContainsKey(detector))
+        {
+            boidsDetection[detector].Add(detected);
+        }
+        else
+        {
+            List<BoidController> detectedList = new List<BoidController>();
+            detectedList.Add(detected);
+            boidsDetection.Add(detector, detectedList);
+        }
+    }
+
+    private bool inRange(BoidController b1, BoidController b2)
+    {
+        float distance = Vector3.Distance(b1.transform.position, b2.transform.position);
+        return distance < detectionRange;
+    }
+
+    private bool isDetected(BoidController detector, BoidController detectee)
+    {
+        float angle = Vector3.Angle(detector.transform.forward, detectee.transform.position - detector.transform.position);
+        return angle < detectionAngle;
+    }
+
+
+    public List<BoidController> getInRangeBoids(BoidController meSelf)
+    {
+        List<BoidController> list = new List<BoidController>();
+
+        if (boidsDetection.TryGetValue(meSelf, out list))
+        {
+            return boidsDetection[meSelf];
+        }
+        return null;
+    }
+
+    private void moveCamera()
+    {
+        cam.transform.LookAt(boidsList[0].transform);
+    }
+
+}
