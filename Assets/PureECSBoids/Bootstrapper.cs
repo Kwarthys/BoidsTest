6acbe097b6a854045104d98dc6148faf6d82cb9e
Kwarthys <Kwarthys@gmail.com>
2019-03-26 12:33:47 +0100

    First step of Pure ECS
    Got some dumbass boids, but it's a good start !

---
+++ Assets/PureECSBoids/Bootstrapper.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Rendering;
+
+public class Bootstrapper : MonoBehaviour
+{
+    public float spawnArea = 50;
+
+    public int nbAgents = 10;
+
+    public Mesh boidMesh;
+    public Material boidMaterial;
+
+    void Start()
+    {
+        EntityManager entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        Unity.Mathematics.Random random = new Unity.Mathematics.Random(666);
+
+        for(int i = 0; i < nbAgents; ++i)
+        {
+            Entity boidEntity = entityManager.CreateEntity(
+            ComponentType.Create<PureBoidData>(),
+            ComponentType.Create<Position>(),
+            ComponentType.Create<Rotation>(),
+            ComponentType.Create<RenderMesh>());
+
+            entityManager.SetComponentData(boidEntity, new PureBoidData
+            {
+                speed = 0.2f
+            });
+
+            entityManager.SetComponentData(boidEntity, new Position
+            {
+                Value = new float3(random.NextFloat(-spawnArea, spawnArea), random.NextFloat(-spawnArea, spawnArea), random.NextFloat(-spawnArea, spawnArea))
+            });
+
+            entityManager.SetComponentData(boidEntity, new Rotation
+            {
+                Value = quaternion.Euler(
+                    random.NextFloat(-180.0f, 180.0f),
+                    random.NextFloat(-180.0f, 180.0f),
+                    random.NextFloat(-180.0f, 180.0f)
+                )
+            });
+
+            entityManager.SetSharedComponentData(boidEntity, new RenderMesh
+            {
+                mesh = boidMesh,
+                material = boidMaterial
+            });
+        }        
+    }
+}
+
---
+++ Assets/PureECSBoids/PureBoidSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public class PureBoidSystem : JobComponentSystem
+{
+    private struct BoidMovementJob : IJobProcessComponentData<PureBoidData, Position, Rotation>
+    {
+        public void Execute(ref PureBoidData data, ref Position pos, ref Rotation rot)
+        {
+            float3 forward = math.mul(rot.Value, new float3(0.0f, 1.0f, 0.0f));
+            pos.Value += data.speed * forward;
+
+            rot.Value = math.mul(rot.Value, quaternion.RotateX(0.1f));
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        BoidMovementJob job = new BoidMovementJob{};
+        return job.Schedule(this, inputDeps);
+    }
+    
+}
+
